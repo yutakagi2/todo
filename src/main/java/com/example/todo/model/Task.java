fac6a5632d9646470950a69c136f0caf1b912df6
yu.yakagi <yu.takagi@avantcorp.com>
2025-06-13 18:28:42 +0900

    フロントエンドの課題完了

--- src/main/java/com/example/todo/model/Task.java
+++ src/main/java/com/example/todo/model/Task.java
@@ -1,15 +1,18 @@
 package com.example.todo.model;
-
+ 
 import jakarta.persistence.*;
 import lombok.Data;
-
+ 
 @Entity
-@Data // Lombokを使用　（使用しない場合はgetter/setterを自分で定義）
+@Data // Lombokを使用（使用しない場合はgetter/setterを自分で定義）
 public class Task {
-
+ 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    @Column(nullable = false)
     private String title;
-
+ 
     private boolean completed = false;
 }
--- src/test/java/com/example/todo/TodoApplicationTest.java
+++ src/test/java/com/example/todo/TodoApplicationTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-public class TodoApplicationTests {
+public class TodoApplicationTest {
 
     @Autowired
     private TaskRepository taskRepository;
